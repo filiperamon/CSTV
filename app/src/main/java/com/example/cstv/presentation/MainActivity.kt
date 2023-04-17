cd5d0f5f30c918dbed1831ca3f73e0a157bf75a2
filipe.ramon <bliteti.filipe@unidas.com.br>
2023-04-17 15:33:32 -0300

    Config dependeces, model core and my applycation

--- app/src/main/java/com/example/cstv/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cstv/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.cstv
+package com.example.cstv.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cstv.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
